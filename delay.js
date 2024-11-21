21122258d2c538b29adf429bb269bc4992e9fdc9
Vitalii Misechko <Vitalii.Misechko@D2L.com>
2024-11-21 20:15:17 +0300

    7 mins job

--- delay.js
+++ delay.js
@@ -1,7 +1,7 @@
 const { argv } = require('node:process');
 
 let duration = 1 * 60 * 1000; // 1 minute
-duration = duration * 1;
+duration = duration * 7;
 duration = duration * 1;
 duration = duration * 1;
 
