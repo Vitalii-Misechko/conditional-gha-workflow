1cb55807d4eff53b44213fe5cc16ea998bc0a3fb
Vitalii Misechko <Vitalii.Misechko@D2L.com>
2024-11-21 21:19:53 +0300

    2 mins

--- delay.js
+++ delay.js
@@ -3,7 +3,7 @@ const { argv } = require('node:process');
 let duration = 1 * 60 * 1000; // 1 minute
 duration = duration * 1;
 duration = duration * 1;
-duration = duration * 5;
+duration = duration * 2;
 
 (async () => {
     await new Promise((resolve) => {
