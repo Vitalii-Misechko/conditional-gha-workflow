ecdffaf04f7659bdddda36b48087faacd0be64ba
Vitalii Misechko <Vitalii.Misechko@D2L.com>
2024-11-25 17:54:32 +0300

    A failing change

--- delay.js
+++ delay.js
@@ -1,7 +1,7 @@
 const { argv } = require('node:process');
 
 const duration = 1 * 20 * 1000; // 20 sec
-
+asdfasfas
 (async () => {
     await new Promise((resolve) => {
         setTimeout(resolve, duration);
